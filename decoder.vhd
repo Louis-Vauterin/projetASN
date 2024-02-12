6a3a6af3999d8ea4cc2f6b1e3833a7cfdef5e16a
Louis Vauterin <louisvauterin@Louiss-MacBook-Pro.local>
2024-02-12 17:03:54 +0100

    fix decoder and add test_decoder

--- decoder.vhd
+++ decoder.vhd
@@ -7,41 +7,38 @@ entity Decoder is
 	PORT(reset : in std_logic;
 		 clock : in std_logic;
 		 instruction : in std_logic_vector(15 downto 0);
-		 addrDest : out std_logic_vector(3 downto 0);
+		 addrDest : out std_logic_vector(2 downto 0);
 		 imm : out std_logic_vector(5 downto 0);
 		 selR : out std_logic_vector(4 downto 0);
-		 addrA : out std_logic_vector(3 downto 0);
-		 addrB : out std_logic_vector(3 downto 0);
+		 addrA : out std_logic_vector(2 downto 0);
+		 addrB : out std_logic_vector(2 downto 0);
+     opcode : out std_logic_vector(4 downto 0);
 		 enable : out std_logic
 	);
 END Decoder;
 
 architecture Decoder_arch of Decoder is
 
-    --signal NB_WAIT:     natural range 0 to 4;       -- nb of operations to wait
-    --signal SP:          natural range 0 to 4095;    -- stack pointer
-
-    signal ins:         std_logic_vector(15 downto 0);
-    signal op:          std_logic_vector(3 downto 0);
-    signal format:      std_logic_vector(1 down to 0)
-    --signal jump_sig:    std_logic;
-    --signal enable_RAM_sig: std_logic;
+    signal op:          std_logic_vector(2 downto 0);
 
 begin
+    process(clock, reset)
+      begin
+      if reset = '1' then
+        null; -- a implementer
+      elsif rising_edge(clock) then
+      -- decode instruction
+      opcode <= instruction(15 downto 11);
+      selR <= instruction(15 downto 11);
+      addrDest <= instruction(10 downto 8);
+      addrA <= instruction(4 downto 2);
+      addrB <= instruction(7 downto 5);
+      
+      imm <= "000000";
 
-    -- decode instruction
-    format <= ins(15 downto 14);
-    op <= ins(13 down to 11);
-    selR <= ins(15 down to 11);
-    addrA <= ins(4 downto 2);
-    addrB <= ins(7 downto 5);
-    addrDest <= ins(10 downto 8);
-    imm <=
-        "000000" & ins(4 downto 0) when ins(15 downto 14) = "01" else
-        ins(5 downto 0) when ins(15 downto 14) = "10";
-
-    -- use
-    use_reg_imm <= '1' when not(format = "01") else '0';
-    use_addrA <= '1' when ins(15 downto 14) = "00" else '0';
-
+      -- use
+      --use_reg_imm <= '1' when not(format = "01") else '0';
+      --use_addrA <= '1' when ins(15 downto 14) = "00" else '0';
+    end if;
+    end process;
 end Decoder_arch;
---
+++ test_decoder.vhd
@@ -0,0 +1,61 @@
+-- testbench for decoder
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity test_decoder is
+end test_decoder;
+
+architecture testbench1 of test_decoder is
+    -- signaux
+	 
+	component decoder is
+	PORT(reset : in std_logic;
+		 clock : in std_logic;
+		 instruction : in std_logic_vector(15 downto 0);
+		 addrDest : out std_logic_vector(2 downto 0);
+		 imm : out std_logic_vector(5 downto 0);
+		 selR : out std_logic_vector(4 downto 0);
+		 addrA : out std_logic_vector(2 downto 0);
+		 addrB : out std_logic_vector(2 downto 0);
+		 enable : out std_logic
+	);
+	END component;
+
+  SIGNAL reset : STD_LOGIC := '0';
+  SIGNAL sig_clock : STD_LOGIC := '0';
+  SIGNAL instruction : STD_LOGIC_VECTOR(15 DOWNTO 0);
+  SIGNAL addrDest : STD_LOGIC_VECTOR(2 DOWNTO 0);
+  SIGNAL imm : STD_LOGIC_VECTOR(5 DOWNTO 0);
+  SIGNAL selR : STD_LOGIC_VECTOR(4 DOWNTO 0);
+  SIGNAL addrA : STD_LOGIC_VECTOR(2 DOWNTO 0);
+  SIGNAL addrB : STD_LOGIC_VECTOR(2 DOWNTO 0);
+  SIGNAL enable : STD_LOGIC;
+
+    
+    begin 
+        sig_clock <= not sig_clock after 5 ns;
+        dut : decoder
+            -- map ports to testbench
+            port map (
+					 reset => reset,
+                    clock => sig_clock,
+                    instruction => instruction,
+					 addrDest => addrDest,
+					 imm => imm,
+					 selR => selR,
+					 addrA => addrA,
+                     addrB => addrB,
+					 enable => enable
+            );
+        
+        process is
+            begin
+            wait until rising_edge(sig_clock);
+            instruction <= "0000001000100000";
+            wait until rising_edge(sig_clock);
+            instruction <= "1000000000000000";
+            end process;
+				
+end architecture testbench1;
