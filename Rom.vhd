3dcb6d6fb62da883b4f8456182ec95dd72866905
Louis Vauterin <louisvauterin@Louiss-MacBook-Pro.local>
2024-02-12 23:04:36 +0100

    Add example program to Rom

--- Rom.vhd
+++ Rom.vhd
@@ -17,7 +17,16 @@ end rom;
 architecture rom_a of rom is
 
 type rom is array(0 to 255) of std_logic_vector(15 downto 0); --16 bits to hold 1 instruction
-rom(0) <= "0000001000100000"; -- test instruction
+-- instructions pour programme de test
+rom(0) <= "1000000000001010"; --  MOV 000 10        --> A = 10
+rom(1) <= "1000000000100101"; --  MOV 001 5         --> B = 5 
+rom(2) <= "1000000001001000"; --  MOV 010 8         --> C =8
+rom(3) <= "0000010000100000"; --  ADD 100 001 000   --> D = A + B
+rom(4) <= "0100110101010011"; --  SUB 101 010 3     --> E = C -3
+rom(5) <= "0010000010010100"; --  AND 000 100 101   --> A = D & E
+rom(6) <= "0010100110010100"; --  OR 001 100 101    --> B = D || E
+rom(7) <= "0101001000100001"; --  RSHIFT 010 001 1  --> C = B // 2
+
 
 signal Data_Rom : rom ;
 
